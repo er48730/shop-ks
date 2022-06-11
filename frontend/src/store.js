22648f5fcd2083533ef0c082605dbc56845d0ffe
Kaltrina Gashi <kaltrinag54@gmail.com>
2022-06-12 01:57:35 +0200

    Delete Product

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducers";
 import { orderCreateReducer, orderDetailsReducer, orderListReducer, orderMineListReducer, OrderPayReducer } from "./reducers/orderReducers";
-import { productCreateReducer, productDetailsReducer, productListReducer, productUpdateReducer } from "./reducers/productReducers";
+import { productCreateReducer, productDeleteReducer, productDetailsReducer, productListReducer, productUpdateReducer } from "./reducers/productReducers";
 import { userDetailsReducer, userRegisterReducer, userSigninReducer, userUpdateProfileReducer } from "./reducers/userReducers";
 
 const initialState = {
@@ -35,6 +35,7 @@ const reducer = combineReducers({
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
   orderList: orderListReducer,
+  productDelete: productDeleteReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
