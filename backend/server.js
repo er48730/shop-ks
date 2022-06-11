255a2d83e83492a344931415fc9174957cbc1f37
Kaltrina Gashi <kaltrinag54@gmail.com>
2022-06-12 00:55:57 +0200

    Upload Product Image

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import productRouter from "./routers/productRouter.js";
 import dotenv from "dotenv";
+import path from 'path';
 import userRouter from "./routers/userRouter.js";
 import orderRouter from "./routers/orderRouter.js";
+import uploadRouter from "./routers/uploadRouter.js";
 
 dotenv.config();
 const app = express();
@@ -16,13 +18,16 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/shop-ks", {
   // useUnifiedTopology: true,
   // useCreateIndex: true,
 });
-
+app.use('/api/uploads', uploadRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
